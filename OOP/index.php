0d57189cdb3aca1e1e46a5544c8ef1527b6a1c3e
Doan Khue <doankhue09@gmail.com>
2019-07-05 20:45:39 +0700

    connect database php7

--- OOP/index.php
+++ OOP/index.php
@@ -3,22 +3,62 @@
 /**
  * 
  */
-class Hero
-{
+// class Hero
+// {
 	
-	public $name;
-	public $power;
+// 	public $name;
+// 	public $power;
 
-	public function kick($someone){
-		return "Kick ".$someone;
+// 	public function kick($someone){
+// 		return "Kick ".$someone;
+// 	}
+// }
+
+// $ironMan = new Hero();
+// $ironMan->name = "Iron Man";
+// $ironMan->power = 120;
+
+// echo $ironMan->name;
+// echo "<br/>";
+// echo $ironMan->kick("Daisy");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+class TotalBalance{
+	public $totalbalance;
+	function __construct($totalbalance){
+		$this->totalbalance = $totalbalance;
+	}
+	public function getPercentFromTotal($money){
+		return ($money/$this->totalbalance) ;
 	}
 }
 
-$ironMan = new Hero();
-$ironMan->name = "Iron Man";
-$ironMan->power = 120;
+$trade = new TotalBalance(11520);
+// echo $trade->totalbalance;
+ $amount =   767676*0.21;
 
-echo $ironMan->name;
-echo "<br/>";
-echo $ironMan->kick("Daisy");
-?>
+// 200000 * $trade->getPercentFromTotal(8000);
+echo $amount;
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, amount)
+VALUES ('dasd', 'asd', $amount)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
